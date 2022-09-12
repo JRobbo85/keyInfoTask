58d8805dc1d5d6c24d6ed397094d9a66c26982f7
JRobbo85 <jrobertson85@hotmail.co.uk>
2022-09-12 15:57:23 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-document.addEventListener("keydown", (event) => {
     const welcome = document.getElementById("welcome")
     const codeDisplay = document.getElementById("codeResult")
     const whichDisplay = document.getElementById("whichResult")
+    
+    
+    document.addEventListener("keydown", (event) => {
     welcome.textContent = `${event.key}` 
     codeDisplay.textContent = `${event.code}`
     whichDisplay.textContent = `${event.which}`
