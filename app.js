9486a678fac3a9f50b8b31faf3fd9dd158b29651
JRobbo85 <jrobertson85@hotmail.co.uk>
2022-09-12 15:16:30 +0100

    update

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+
+document.addEventListener("keydown", (event) => {
+    const welcome = document.getElementById("welcome")
+    const codeDisplay = document.getElementById("codeResult")
+    const whichDisplay = document.getElementById("whichResult")
+    welcome.textContent = `${event.key}` 
+    codeDisplay.textContent = `${event.code}`
+    whichDisplay.textContent = `${event.which}`
+    document.getElementById("output").appendChild(welcome)
+})
